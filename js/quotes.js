da7d2cc23bc4939edbe8fd364cc536353b98757d
James Peilow <james.peilow@live.co.uk>
2017-10-14 20:24:38 +0100

    Updated function for local json
    Script updated for retrieving quotes from a local .json file

--- js/quotes.js
+++ js/quotes.js
@@ -1,16 +1,23 @@
 var twitterUrl = "https://twitter.com/intent/tweet?text=";
+function selectQuote(quoteArray) {
+	var randomIndex = Math.floor(quoteArray.length*Math.random());
+	return quoteArray[randomIndex];
+}
+
+
 function getQuotes() {
 	$.ajax({
-		url: "http://test.rjfgo.xyz/udemy/lectures/php/quotescraper.php",
-		async: false,
+		async:false,
+		url: "quotes.json",
 		dataType: "json",
-		success:(function(result){
+		success:(function(data){
+			var quote = selectQuote(data);
 			$(".quote-text").fadeOut("slow", function() {
-				$("#quote").text(result.content);
-				$("#author").text(result.author);
-				$("#year").text(result.year);
+				$("#quote").text(quote.content);
+				$("#author").text(quote.author);
+				$("#year").text(quote.year);
 				$(".quote-text").fadeIn("slow");
-				$("#tweet a").attr("href", twitterUrl+result.content+" - "+result.author+" %23minimalism");	
+				$("#tweet a").attr("href", twitterUrl+quote.content+" - "+quote.author+" %23minimalism");	
 			});
 		})
 	})
